f9ed18be0ed5c73e37fd0ace16e35d3e4143b6f9
김나영 <ny3021@gmail.con>
2022-04-25 15:52:50 +0900

    [Rename]컴포넌트 폴더

---
+++ frontend/function/test2.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Test2: FC = () => {
+  return <div></div>;
+};
+
+export default Test2;
---
+++ frontend/pages/update/test.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const Test1: FC = () => {
+  return <div></div>;
+};
+
+export default Test1;
