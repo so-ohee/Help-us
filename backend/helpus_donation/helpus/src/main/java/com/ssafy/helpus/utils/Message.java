6fd72a2aef8b6fc6e2e746571b45a2cd5b12981c
sohee960328 <sohee960328@gmail.com>
2022-04-28 16:21:25 +0900

    Feat: 기부 서버 초기 세팅

---
+++ backend/helpus_donation/helpus/src/main/java/com/ssafy/helpus/utils/Message.java
@@ -0,0 +1,51 @@
+package com.ssafy.helpus.utils;
+
+public class Message {
+    public static final String DONATION_REGISTER_SUCCESS = "기부 등록 성공";
+    public static final String DONATION_REGISTER_FAIL = "기부 등록 실패";
+    public static final String DONATION_NOT_FOUND = "기부 글 없음";
+    public static final String DONATION_FIND_SUCCESS = "기부 조회 성공";
+    public static final String DONATION_FIND_FAIL = "기부 조회 실패";
+    public static final String DONATION_UPDATE_SUCCESS = "기부 수정 성공";
+    public static final String DONATION_UPDATE_FAIL = "기부 수정 실패";
+    public static final String DONATION_END_SUCCESS = "기부 마감 성공";
+    public static final String DONATION_END_FAIL = "기부 마감 실패";
+
+    public static final String DONATION_APPLY_SUCCESS = "기부 신청 성공";
+    public static final String DONATION_APPLY_FAIL = "기부 신청 실패";
+    public static final String APPLY_FIND_SUCCESS = "기부 조회 성공";
+    public static final String APPLY_FIND_FAIL = "기부 조회 실패";
+    public static final String APPLY_NOT_FOUND = "기부 내역 없음";
+
+    public static final String INVOICE_REGISTER_SUCCESS = "송장 번호 등록 성공";
+    public static final String INVOICE_REGISTER_FAIL = "송장 번호 등록 실패";
+    public static final String INVOICE_UPDATE_SUCCESS = "송장 번호 수정 성공";
+    public static final String INVOICE_UPDATE_FAIL = "송장 번호 수정 실패";
+    public static final String INVOICE_FIND_SUCCESS = "송장 조회 성공";
+    public static final String INVOICE_FIND_FAIL = "송장 조회 실패";
+    public static final String INVOICE_NOT_FOUND = "송장 내역 없음";
+
+    public static final String DELIVERY_UPDATE_SUCCESS = "배송 현황 수정 성공";
+    public static final String DELIVERY_UPDATE_FAIL = "배송 현황 수정 실패";
+    public static final String DELIVERY_FIND_SUCCESS = "배송 현황 조회 성공";
+    public static final String DELIVERY_FIND_FAIL = "배송 현황 조회 실패";
+    public static final String DELIVERY_NOT_FOUND = "배송 현황 없음";
+
+    public static final String COMMENT_REGISTER_SUCCESS = "댓글 등록 성공";
+    public static final String COMMENT_REGISTER_FAIL = "댓글 등록 실패";
+    public static final String COMMENT_NOT_FOUND = "댓글 없음";
+    public static final String COMMENT_FIND_SUCCESS = "댓글 조회 성공";
+    public static final String COMMENT_FIND_FAIL = "댓글 조회 실패";
+    public static final String COMMENT_DELETE_SUCCESS = "댓글 삭제 성공";
+    public static final String COMMENT_DELETE_FAIL = "댓글 삭제 실패";
+
+    public static final String CONFIRM_REGISTER_SUCCESS = "후기 등록 성공";
+    public static final String CONFIRM_REGISTER_FAIL = "후기 등록 실패";
+    public static final String CONFIRM_UPDATE_SUCCESS = "후기 수정 성공";
+    public static final String CONFIRM_UPDATE_FAIL = "후기 수정 실패";
+    public static final String CONFIRM_FIND_SUCCESS = "후기 조회 성공";
+    public static final String CONFIRM_FIND_FAIL = "후기 조회 실패";
+    public static final String CONFIRM_NOT_FOUND = "후기 없음";
+    public static final String CONFIRM_MEMBER_FIND_SUCCESS = "명단 조회 성공";
+    public static final String CONFIRM_MEMBER_FIND_FAIL = "명단 조회 실패";
+}
