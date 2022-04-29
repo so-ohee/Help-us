8b1cd6001ac01b2f27af0b9fbaad480e5f626dfd
helen121027 <helen121027@gmail.com>
2022-04-29 09:35:30 +0900

    Feat: 메인 페이지 수정 중

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import { Box, Grid, Tab, Typography, Stack } from "@mui/material/";
+
+const Footer = () => {
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Box sx={{height: 200, width: '100%', backgroundColor: '#FCF8F0'}}>
+
+      </Box>
+    </Grid>
+  )
+}
+
+export default Footer;
