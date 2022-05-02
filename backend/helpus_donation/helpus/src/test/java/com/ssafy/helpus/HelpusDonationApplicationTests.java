56ef8729799a17af8d625b7a16f3438c8de7ee60
최다운 <choidawoon96@naver.com>
2022-05-02 06:39:28 +0000

    Update HelpusDonationApplicationTests.java

--- backend/helpus_donation/helpus/src/test/java/com/ssafy/helpus/HelpusDonationApplicationTests.java
+++ backend/helpus_donation/helpus/src/test/java/com/ssafy/helpus/HelpusDonationApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.helpus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class HelpusDonationApplicationTests {
 
 	@Test
