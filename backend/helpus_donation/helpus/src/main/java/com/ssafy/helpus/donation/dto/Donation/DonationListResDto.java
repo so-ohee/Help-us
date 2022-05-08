0de1cb7e87de6e258de672254744e285332ecb74
sohee960328 <sohee960328@gmail.com>
2022-05-08 22:09:01 +0900

    Rename: 안쓰는 import 제거

--- backend/helpus_donation/helpus/src/main/java/com/ssafy/helpus/donation/dto/Donation/DonationListResDto.java
+++ backend/helpus_donation/helpus/src/main/java/com/ssafy/helpus/donation/dto/Donation/DonationListResDto.java
@@ -1,6 +1,5 @@
 package com.ssafy.helpus.donation.dto.Donation;
 
-import com.ssafy.helpus.donation.dto.Donation.DonationListProductResDto;
 import com.ssafy.helpus.donation.enumClass.DonationStatus;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
