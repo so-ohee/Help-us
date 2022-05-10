9b1fc848c5031c1efac19153ba604c5fb89ea081
sohee960328 <sohee960328@gmail.com>
2022-05-10 11:54:26 +0900

    Rename: DonationController

--- backend/helpus_donation/helpus/src/main/java/com/ssafy/helpus/donation/dto/Donation/DonationReqDto.java
+++ backend/helpus_donation/helpus/src/main/java/com/ssafy/helpus/donation/dto/Donation/DonationReqDto.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.List;
 
