01418251787cc20dcd7ffe025a3a5bb01e75e7fe
ChoiDaWoon <choidawoon960717@gmail.com>
2022-05-11 15:41:40 +0900

    Fix 봉사신청

--- backend/helpus_volunteer/helpus/src/main/java/com/ssafy/helpus/volunteer/repository/VolunteerApplyRepository.java
+++ backend/helpus_volunteer/helpus/src/main/java/com/ssafy/helpus/volunteer/repository/VolunteerApplyRepository.java
@@ -1,10 +1,13 @@
 package com.ssafy.helpus.volunteer.repository;
 
+import com.ssafy.helpus.volunteer.entity.Volunteer;
 import com.ssafy.helpus.volunteer.entity.VolunteerApply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VolunteerApplyRepository extends JpaRepository <VolunteerApply, Long>{
-
+     List<VolunteerApply> findByVolunteer(Volunteer volunteer);
 }
