b5ba28ef6aaad5b36ad60a9032f3b56b16d5c542
sohee960328 <sohee960328@gmail.com>
2022-05-11 15:03:23 +0900

    Rename: 주석 제거

--- backend/helpus/helpus/src/main/java/com/ssafy/helpus/dto/Desk/DeskListResDto.java
+++ backend/helpus/helpus/src/main/java/com/ssafy/helpus/dto/Desk/DeskListResDto.java
@@ -6,7 +6,6 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
-import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @ApiModel(value = "고객센터 목록")
