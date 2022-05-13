6286af9a42a9435dd4f288c991a3ebac7ee351f3
sohee960328 <sohee960328@gmail.com>
2022-05-13 12:40:45 +0900

    Feat: 고객센터 댓글 삭제 오류 수정

--- backend/helpus/helpus/src/main/java/com/ssafy/helpus/repository/CommentRepository.java
+++ backend/helpus/helpus/src/main/java/com/ssafy/helpus/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.ssafy.helpus.repository;
 
+import com.ssafy.helpus.model.HelpDesk;
 import com.ssafy.helpus.model.HelpDeskComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<HelpDeskComment, Long> {
+    boolean existsByHelpDesk(HelpDesk helpDesk);
 }
