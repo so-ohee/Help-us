b49d7719bf2ebab71307513d62ac4f6d57d6b7c2
sohee960328 <sohee960328@gmail.com>
2022-05-17 07:43:55 +0900

    Feat: 재능기부 등록페이지 목록으로 이동 변경

--- frontend/pages/create/talent.tsx
+++ frontend/pages/create/talent.tsx
@@ -248,7 +248,7 @@ const Talent: FC = () => {
       <Grid container justifyContent="center" alignItems="center">
         <Stack sx={{ minWidth: 1200 }}>
           <Box sx={{ display: 'flex', justifyContent: 'flex-end'}}>
-            <CustomButton variant="contained" href="/share">
+            <CustomButton variant="contained" onClick={() => history.back()}>
               목록으로
             </CustomButton>
           </Box>
