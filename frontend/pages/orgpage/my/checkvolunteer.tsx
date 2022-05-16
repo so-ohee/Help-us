425961b17971b398c4251ee840cc7b23a0c5cb8a
sohee960328 <sohee960328@gmail.com>
2022-05-16 09:59:26 +0900

    Fix: 봉사 현황 조회

--- frontend/pages/orgpage/my/checkvolunteer.tsx
+++ frontend/pages/orgpage/my/checkvolunteer.tsx
@@ -16,7 +16,10 @@ import {
   Button,
 } from "@mui/material";
 import { tableCellClasses } from "@mui/material/TableCell";
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
+
+// api
+import { getInquiryApplyList } from "function/axios";
 
 const CustomButton = styled(Button)({
   backgroundColor: "#5B321E",
@@ -166,20 +169,20 @@ const dummyData = [
 const IsFact = ({ fact }) => {
   // const fact = props.fact;
 
-  if (fact === null) {
+  if (fact === 0) {
     return (
       <>
         <CustomButton sx={{ width: 40, height: 30, mr: 2 }}>참석</CustomButton>
         <CustomButton2 sx={{ width: 40, height: 30 }}>불참</CustomButton2>
       </>
     );
-  } else if (fact === true) {
+  } else if (fact === 1) {
     return (
       <>
         <Typography>참석</Typography>
       </>
     );
-  } else if (fact === false) {
+  } else if (fact === 2) {
     return (
       <>
         <Typography>불참</Typography>
@@ -189,6 +192,29 @@ const IsFact = ({ fact }) => {
 };
 
 const orgpageMyCheckVolunteer: FC = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [inquiryApplyList, setInquiryApplyList] = useState<any>(null);
+
+  // pagination
+  const [curPage, setCurPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const paginate = (pageNumber) => setCurPage(pageNumber);
+
+  const params = {
+    memberId: ""
+  };
+
+  useEffect(() => {
+    params.memberId = localStorage.getItem("id");
+    getInquiryApplyList(params).then((res) => {
+      console.log(res.data.listApply.volunteerApplyId);
+      setInquiryApplyList(res.data.listApply);
+      setTotalPages(res.data.totalPage);
+      // console.log("data는", reviewList);
+      setLoading(true);
+    });
+  }, [curPage]);
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -226,22 +252,22 @@ const orgpageMyCheckVolunteer: FC = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {dummyData.map((data) => (
-                  <StyledTableRow key={data.donationApplyId}>
+                {inquiryApplyList && inquiryApplyList.map((data) => (
+                  <StyledTableRow key={data.volunteerApplyId}>
                     <StyledTableCell align="center">
-                      {data.donationApplyId}
+                      {data.volunteerApplyId}
                     </StyledTableCell>
                     <StyledTableCell align="center" sx={{ width: 400 }}>
                       {data.title}
                     </StyledTableCell>
                     <StyledTableCell align="center">
-                      {data.donationDate}
+                      {data.volDate}
                     </StyledTableCell>
                     <StyledTableCell align="center">
                       {data.name}
                     </StyledTableCell>
                     <StyledTableCell align="center">
-                      <IsFact fact={data.fact} />
+                      <IsFact fact={data.status} />
                       {/* <CustomButton sx={{ width: 40, height: 30, mr: 2 }}>
                         참석
                       </CustomButton>
